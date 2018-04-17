c5a2c96bde5943543180c829ce21f11bd07a081c
Ben Johnson <bjohnson@binarylogic.com>
2018-04-17 15:21:36 -0400

    Prepare v0.0.2 release

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ManifoldcoSignature.MixProject do
 
   @source_url "https://github.com/timberio/elixir-manifoldco-signature"
   @homepage_url "https://github.com/timberio/elixir-manifoldco-signature"
-  @version "0.0.1"
+  @version "0.0.2"
 
   def project do
     [
